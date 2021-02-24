a895a631a06d002cf26185db4042b895a43ed9c1
AZAZ78 <alexander.zeh@gmx.de>
2021-02-24 10:58:04 +0100

    First version

--- __init__.py
+++ __init__.py
@@ -5,9 +5,9 @@ class DigitalWife(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('wife.digital.intent')
+    @intent_file_handler('wife.intent')
     def handle_wife_digital(self, message):
-        self.speak_dialog('wife.digital')
+        self.speak_dialog('wife')
 
 
 def create_skill():
